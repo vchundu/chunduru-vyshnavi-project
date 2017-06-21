fc7dd98d04ed5147bcbe30afb6b356b68149f55f
Vyshnavi Chunduru <chunduru.v@husky.neu.edu>
2017-06-21 16:25:05 -0400

    fixed heroku

--- project/app.js
+++ project/app.js
@@ -8,7 +8,15 @@ app.use(bodyParser);
 
 // Mongo
 var connectionString = 'mongodb://localhost/webdev_assignment'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds141108.mlab.com:41108/heroku_m0fgmn47';
+}
 mongoose.connect(connectionString);
+
+
 
 // Local Requires
 require('./services/user.service.server');
